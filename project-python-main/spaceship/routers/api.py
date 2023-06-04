e630a39d09c207fde8a69a611bb99d475ce0a8be
Dolaran <gyv220427@gmail.com>
2023-06-04 21:45:04 +0300

    feat: change api.py

--- project-python-main/spaceship/routers/api.py
+++ project-python-main/spaceship/routers/api.py
@@ -8,6 +8,8 @@ def hello_world() -> dict:
         "name": "Yurii",
         "second_name": "Grygorash",
         "age": 19,
+        "alive": "almost dead",
+        "shizofrenia": "Maybe have shizofrenia",
         "hobby": ["Be late", "Strategy games", "swimming"]
     }
     
