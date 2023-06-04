a3b10b5a9ca134fe535b322d6543fb8540d9f851
Dolaran <gyv220427@gmail.com>
2023-06-04 21:16:09 +0300

    feat: change api (for second image)

--- project-python-main/spaceship/routers/api.py
+++ project-python-main/spaceship/routers/api.py
@@ -2,7 +2,13 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
 @router.get('')
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    data = {
+        "name": "Yurii",
+        "second_name": "Grygorash",
+        "age": 19,
+        "hobby": ["Be late", "Strategy games", "swimming"]
+    }
+    
+    return data
